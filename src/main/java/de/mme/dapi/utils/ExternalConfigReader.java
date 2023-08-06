a8b9b57815647ec01db4cfccfbed06712660bf3d
MrMame <mm@mail.de>
2023-08-06 10:58:07 +0200

    add logger for class

--- src/main/java/de/mme/dapi/utils/ExternalConfigReader.java
+++ src/main/java/de/mme/dapi/utils/ExternalConfigReader.java
@@ -1,30 +1,49 @@
 package de.mme.dapi.utils;
 
 import de.mme.dapi.Main;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Paths;
 import java.util.Properties;
 
 public class ExternalConfigReader {
 
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     private static final String CONFIGFOLDER = "externalConfigs/";
     private Properties configProps = null;
+    private String completeConfigFilename;
 
 
 
 
 
-    public ExternalConfigReader(String Configfilename) throws IOException {
-        this.configProps = readExternalConfigProperties(Configfilename);
+
+    public ExternalConfigReader(String configFilename) throws IOException {
+        try{
+            this.configProps = readExternalConfigProperties(configFilename);
+        }catch (IOException e){
+            logger.error("IOException while trying to read configFilename '" + this.completeConfigFilename + "'");
+            throw e;
+        }catch(Exception e){
+            logger.error("General Exception while trying to read configFilename '" + this.completeConfigFilename + "'");
+            throw e;
+        }
+
     }
 
     public String readProperty(String propertyName){
-        return configProps.getProperty(propertyName);
+        String returnValue = configProps.getProperty(propertyName);
+        if(returnValue == null) {
+            logger.warn("There is no property found named '"
+                    + propertyName + "' in external config file '"
+                    + this.completeConfigFilename + "'");
+        }
+        return returnValue;
     }
 
 
@@ -32,6 +51,7 @@ public class ExternalConfigReader {
 
     private Properties readExternalConfigProperties(String Filename) throws IOException {
         String configFilename = getJarPath() + CONFIGFOLDER + Filename;
+        this.completeConfigFilename = configFilename;
 
         Properties discordProps = new Properties();
         try(FileInputStream fis = new FileInputStream(configFilename)){
