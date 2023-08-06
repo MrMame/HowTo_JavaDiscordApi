4ddf80344e2e59351291bce157103774cb3cbea1
MrMame <mm@mail.de>
2023-08-06 11:38:05 +0200

    Add Discord Event Package

---
+++ src/main/java/de/mme/dapi/discord/events/ReadyListener.java
@@ -0,0 +1,19 @@
+package de.mme.dapi.discord.events;
+
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.EventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReadyListener implements EventListener
+{
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void onEvent(GenericEvent genericEvent) {
+        if (genericEvent instanceof ReadyEvent)
+            this.logger.info("FIRED ReadyEvent - API is ready");
+    }
+}
