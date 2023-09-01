c9a10f97e34978b75c9baa9171498a9cfc5e1e69
MrMame <mm@mail.de>
2023-09-01 19:09:05 +0200

    Add Button Event handlers

--- src/main/java/de/mme/dapi/examples/messages/MessageWithButtonsMain.java
+++ src/main/java/de/mme/dapi/examples/messages/MessageWithButtonsMain.java
@@ -9,6 +9,7 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
@@ -31,6 +32,15 @@ import java.util.Collections;
 
 public class MessageWithButtonsMain extends ListenerAdapter
 {
+
+    public static final String BTN_OK_ID = "btn-ok-id";
+    public static final String BTN_WHAT_ID = "btn-what-id";
+    public static final String BTN_NO_ID = "btn-no-id";
+    public static final String BTN_ALLOW_ID = "btn-allow-id";
+    public static final String BTN_DENY_ID = "btn-deny-id";
+    public static final String BTN_DENYREASON_ID = "btn-denyreason-id";
+    public static final String BTN_ALERT_ID = "btn-alert-id";
+
     public static void main(String[] args) throws IOException {
 
         String discordToken = new ExternalConfigReader("discordSettings.properties").readProperty("settings.discord.token");
@@ -48,6 +58,34 @@ public class MessageWithButtonsMain extends ListenerAdapter
         ).queue();
     }
 
+
+    @Override
+    public void onButtonInteraction (ButtonInteractionEvent event){
+        if (event.getComponentId().equals(BTN_OK_ID)){
+            event.reply("Button pressed: " + "OK").queue();
+        }
+        if (event.getComponentId().equals(BTN_ALERT_ID)) {
+            event.reply("Button pressed: " + "ALERT").queue();
+        }
+        if (event.getComponentId().equals(BTN_DENYREASON_ID)) {
+            event.reply("Button pressed: " + "DENY REASON").queue();
+        }
+        if (event.getComponentId().equals(BTN_DENY_ID)) {
+            event.reply("Button pressed: " + "DENY").queue();
+        }
+        if (event.getComponentId().equals(BTN_ALLOW_ID)) {
+            event.reply("Button pressed: " + "ALLOW").queue();
+        }
+        if (event.getComponentId().equals(BTN_NO_ID)) {
+            event.reply("Button pressed: " + "NO").queue();
+        }
+        if (event.getComponentId().equals(BTN_WHAT_ID)) {
+            event.reply("Button pressed: " + "WHAT").queue();
+        }
+
+
+    }
+
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event)
     {
@@ -56,9 +94,9 @@ public class MessageWithButtonsMain extends ListenerAdapter
             case "msgbtn":
 
                 // Create thre Buttons. Ok with Primary Style, and What and No with Secondary Style
-                Button btnOk = Button.primary("btn-ok-id","OK");
-                Button btnWhat = Button.secondary("btn-what-id","What?!?!");
-                Button btnNo = Button.secondary("btn-no-id","NO");
+                Button btnOk = Button.primary(BTN_OK_ID,"OK");
+                Button btnWhat = Button.secondary(BTN_WHAT_ID,"What?!?!");
+                Button btnNo = Button.secondary(BTN_NO_ID,"NO");
 
                 // Build the message, including Text and buttons
                 MessageCreateBuilder messageCreateBuilder = new MessageCreateBuilder();
@@ -81,10 +119,10 @@ public class MessageWithButtonsMain extends ListenerAdapter
 
                 // ADD BUTTONS --------------------------------------------------------------------------------
                 // Create 4 buttons and add them to the MessageBuilder
-                Button ButtonAllow = Button.primary("btn-allow-id","Allow");
-                Button ButtonDeny = Button.secondary("btn-deny-id","Deny");
-                Button ButtonDenyWithReason = Button.secondary("btn-denyreason-id","Deny Reason");
-                Button ButtonAlert = Button.danger("btn-alert-id","ALERT!");
+                Button ButtonAllow = Button.primary(BTN_ALLOW_ID,"Allow");
+                Button ButtonDeny = Button.secondary(BTN_DENY_ID,"Deny");
+                Button ButtonDenyWithReason = Button.secondary(BTN_DENYREASON_ID,"Deny Reason");
+                Button ButtonAlert = Button.danger(BTN_ALERT_ID,"ALERT!");
 
                 msgBuilder.setActionRow(ButtonAllow,ButtonDeny,ButtonDenyWithReason,ButtonAlert);
 
